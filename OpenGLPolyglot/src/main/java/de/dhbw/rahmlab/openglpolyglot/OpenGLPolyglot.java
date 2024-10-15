ca70f0da2eea59d4ea10b6f2970f4dc262e3366d
marc <siegfarth03@gmail.com>
2024-10-15 16:18:07 +0200

    add hello world project

---
+++ OpenGLPolyglot/src/main/java/de/dhbw/rahmlab/openglpolyglot/OpenGLPolyglot.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package de.dhbw.rahmlab.openglpolyglot;
+
+/**
+ *
+ * @author marc
+ */
+public class OpenGLPolyglot {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
